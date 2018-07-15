c8a4abe6842c72da251d85a8545213979b5b7720
RocketHcgs <rockethcgs@rhw-team.com>
2018-07-15 09:56:21 +0800

    Updated.

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# HUNT
-仅仅是用来练习CSS和JS的=_=||
+# HUNT!  
+仅仅是用来练习CSS和JS的
+
+# 用途  
+没有什么卵用，可能仅仅是好看（如果你觉得它好看的话）
+
+#DEMO  
+[HUNT!](http://hunt.rhw-team.com/)
+
+#说明  
+- 可以改变页面背景图
+- 可以改变背景颜色，使用CSS线性渐变函数(linear-gradient)
+- 背景图与颜色保存在Cookie中(180天)
+- 如果设置了背景图，颜色将被覆盖
+
+#Author  
+© [RocketHcgs](http://rockethcgs.me/), Released under the MIT License.
---
+++ js.js
@@ -0,0 +1,127 @@
+/*!
+ * HUNT!
+ * JavaScript
+ * License MIT
+ */
+
+function setCookie(c_name,value,expiredays) {
+	var exdate=new Date();
+	exdate.setDate(exdate.getDate()+expiredays)
+	document.cookie=c_name+ "=" +
+	escape(value) +
+	((expiredays==null) ? "" : "; expires="+exdate.toGMTString());
+}
+function getCookie(c_name) {
+	if (document.cookie.length>0) {
+		c_start=document.cookie.indexOf(c_name + "=");
+		if (c_start!=-1) {
+			c_start=c_start + c_name.length+1;
+			c_end=document.cookie.indexOf(";",c_start);
+			if (c_end==-1) c_end=document.cookie.length;
+			return unescape(document.cookie.substring(c_start,c_end));
+		}
+	}
+	return "";
+}
+//Google搜索
+function doGoogle() {
+	var hunt = document.getElementsByName("wd")[0].value;
+	location.href="https://www.google.com/search?q="+hunt;
+}
+//显示/隐藏背景图改变器
+var bgChangerStat = false;
+function bgChanger() {
+	if(bgChangerStat == false) {
+		document.getElementById("bgchanger").style.top="0";
+		bgChangerStat = true;
+	} else {
+		document.getElementById("bgchanger").style.top="-60px";
+		bgChangerStat = false;
+	}
+}
+//显示/隐藏颜色改变器
+var colorChangerStat = false;
+function colorChanger() {
+	if(colorChangerStat == false) {
+		document.getElementById("colorchanger").style.display="block";
+		colorChangerStat = true;
+	} else {
+		document.getElementById("colorchanger").style.display="none";
+		colorChangerStat = false;
+	}
+}
+//改变背景图
+function changeBackground() {
+	var bgURL = document.getElementById("bgurl").value;
+	var bgColor1 = getCookie("bgColor1");
+	var bgColor2 = getCookie("bgColor2");
+	if(bgURL == "" && bgColor1 != "") {
+		document.body.style="background:linear-gradient(to right,"+bgColor1+","+bgColor2+");";
+		document.getElementsByTagName("meta")[2].content=bgColor1;
+		setCookie("bgurl","",-1);
+	} else if(bgURL != "") {
+		document.body.style.background="url("+bgURL+") 60% 0% / cover";
+		document.getElementsByTagName("meta")[2].content="#fff";
+		setCookie("bgurl",bgURL,180);
+	} else if(bgURL == "" && bgColor1 == "") {
+		document.body.style="background:linear-gradient(to right,#f76b1d,#fad961);";
+		document.getElementsByTagName("meta")[2].content="#f8a240";
+		setCookie("bgurl","",-1);
+	}
+}
+//改变背景颜色
+function changeColor() {
+	var bgColor1 = document.getElementById("bgcolor1").value;
+	var bgColor2 = document.getElementById("bgcolor2").value;
+	if(getCookie("bgurl") == "") {
+		document.body.style="background:linear-gradient(to right,"+bgColor1+","+bgColor2+");";
+		document.getElementsByTagName("meta")[2].content=bgColor1;
+	}
+	setCookie("bgColor1",bgColor1,180);
+	setCookie("bgColor2",bgColor2,180);
+}
+//设为默认颜色
+function setDefault() {
+	document.body.style="background:linear-gradient(to right,#f76b1d,#fad961);";
+	document.getElementsByTagName("meta")[2].content="#f8a240";
+	setCookie("bgColor1","",-1);
+	setCookie("bgColor2","",-1);
+}
+
+window.onload = function() {
+	//背景颜色读取
+	if(getCookie("bgColor1")!="") {
+		var bgColor1 = getCookie("bgColor1");
+		var bgColor2 = getCookie("bgColor2");
+		document.getElementById("bgcolor1").value=bgColor1;
+		document.getElementById("bgcolor2").value=bgColor2;
+		if(bgColor1 == bgColor2) {
+			document.body.style.background=bgColor1;
+		} else {
+			document.body.style="background:linear-gradient(to right,"+bgColor1+","+bgColor2+");";
+		}
+		document.getElementsByTagName("meta")[2].content=bgColor1;
+	}
+	//读取保存的背景图
+	if(getCookie("bgurl")!="") {
+		var bgURL = getCookie("bgurl");
+		document.getElementById("bgurl").value=bgURL;
+		document.body.style.background="url("+bgURL+") 60% 0% / cover";
+		document.getElementsByTagName("meta")[2].content="#fff";
+	}
+	//关闭bgChanger和colorChanger的操作
+	var bgButtonObj = document.getElementById("bgbtn");
+	var colorButtonObj = document.getElementById("clbtn");
+	var bgChangerObj = document.getElementById("bgchanger");
+	var colorChangerObj = document.getElementById("colorchanger");
+	document.body.addEventListener('click',function(e) {
+	    if(e.target !== bgButtonObj && e.target != colorButtonObj && e.target != bgChangerObj && e.target != colorChangerObj) {
+	        if (!bgButtonObj.contains(e.target) && !colorButtonObj.contains(e.target) && !bgChangerObj.contains(e.target) && !colorChangerObj.contains(e.target)) {
+	            bgChangerStat = true;
+	            colorChangerStat = true;
+	            bgChanger();
+	            colorChanger();
+	        }
+	    }
+	});
+}
